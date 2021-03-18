c62ea0fa2726a2a87836bc188f9e51bb8cb11276
RitaPeixoto <rita.peixoto2000@gmail.com>
2021-03-18 20:18:14 +0000

    Passed public right tests

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -11,8 +11,63 @@ import pt.up.fe.comp.TestUtils;
 
 public class ExampleTest {
     @Test
-    public void testExpression() {
+    public void HelloWorld() {
         String jmmCode = SpecsIo.getResource("fixtures/public/HelloWorld.jmm");
         TestUtils.parse(jmmCode);
 	}
+
+    @Test
+    public void FindMaximum() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/FindMaximum.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void LazySort() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/Lazysort.jmm");
+        TestUtils.parse(jmmCode);
+    }
+    @Test
+    public void Life() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/Life.jmm");
+        TestUtils.parse(jmmCode);
+    }
+    @Test
+    public void MonteCarloPi() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/MonteCarloPi.jmm");
+        TestUtils.parse(jmmCode);
+    }
+    @Test
+    public void QuickSort() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/QuickSort.jmm");
+        TestUtils.parse(jmmCode);
+    }
+    @Test
+    public void Simple() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/Simple.jmm");
+        TestUtils.parse(jmmCode);
+    }
+    @Test
+    public void TicTacToe() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/TicTacToe.jmm");
+        TestUtils.parse(jmmCode);
+    }
+    /*@Test
+    public void TicTacToe2() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/TicTacToe.input");
+        TestUtils.parse(jmmCode);
+    }*/
+    @Test
+    public void While() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/WhileAndIF.jmm");
+        TestUtils.parse(jmmCode);
+    }
+    /*@Test
+    public void While2() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/TicTacToe.input");
+        TestUtils.parse(jmmCode);
+    }*/
+
+
+
 }
