69f6e1775904a44ff11fc9ddbed4d9c8a33c2878
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2021-03-21 10:25:04 +0000

    added prints to the tests

--- test/FailTests.java
+++ test/FailTests.java
@@ -6,37 +6,49 @@ public class FailTests  {
 
     @Test
     public void BlowUp() {
+        System.out.println("--- Printing Blow Up Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/BlowUp.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("-----------------------------\n");
     }
 
     @Test
     public void CompleteWhile() {
+        System.out.println("--- Printing Complete While Test---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/CompleteWhileTest.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("-------------------------------\n");
     }
 
     @Test
     public void LengthError() {
+        System.out.println("--- Printing Length Error Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/LengthError.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("----------------------------------\n");
     }
 
     @Test
     public void MissingRightPar() {
+        System.out.println("--- Printing Missing Right Par Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/MissingRightPar.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("---------------------------------------\n");
     }
 
     @Test
     public void MultipleSequential() {
+        System.out.println("--- Printing Multiple Sequential Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/MultipleSequential.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("-----------------------------------------\n");
     }
 
     @Test
     public void NestedLoop() {
+        System.out.println("--- Printing Nested Loop Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/NestedLoop.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("---------------------------------\n");
     }
 }
--- test/PassingTests.java
+++ test/PassingTests.java
@@ -5,56 +5,74 @@ import pt.up.fe.specs.util.SpecsIo;
 public class PassingTests {
     @Test
     public void HelloWorld() {
+        System.out.println("--- Printing Hello World Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/HelloWorld.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("-----------------------------\n");
     }
 
     @Test
     public void FindMaximum() {
+        System.out.println("--- Printing Find Maximum Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/FindMaximum.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("----------------------------------\n");
     }
 
     @Test
     public void LazySort() {
+        System.out.println("--- Printing Lazy Sort Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/Lazysort.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("-------------------------------\n");
     }
 
     @Test
     public void Life() {
+        System.out.println("--- Printing Life Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/Life.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("--------------------------\n");
     }
 
     @Test
     public void MonteCarloPi() {
+        System.out.println("--- Printing Monte Carlo Pi Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/MonteCarloPi.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("------------------------------------\n");
     }
 
     @Test
     public void QuickSort() {
+        System.out.println("--- Printing Quick Sort Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/QuickSort.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("--------------------------------\n");
     }
 
     @Test
     public void Simple() {
+        System.out.println("--- Printing Simple Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/Simple.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("-----------------------------\n");
     }
 
     @Test
     public void TicTacToe() {
+        System.out.println("--- Printing TicTacToe Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/TicTacToe.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("-----------------------------\n");
     }
 
     @Test
     public void While() {
+        System.out.println("--- Printing While Test ---\n");
         String jmmCode = SpecsIo.getResource("fixtures/public/WhileAndIF.jmm");
         TestUtils.parse(jmmCode);
+        System.out.println("-----------------------------\n");
     }
 }
 
