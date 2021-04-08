7b862ebd9eda17ac3a49d7f2a4fbae3e19221a3b
João Bispo <joaobispo@gmail.com>
2021-04-08 09:30:55 +0100

    Implements .equals() and .hashCode() for Type and Symbol

--- src-lib/pt/up/fe/comp/jmm/analysis/table/Type.java
+++ src-lib/pt/up/fe/comp/jmm/analysis/table/Type.java
@@ -1,32 +1,12 @@
 package pt.up.fe.comp.jmm.analysis.table;
 
-import pt.up.fe.comp.jmm.JmmNode;
-
-import java.awt.image.Kernel;
-import java.util.List;
-
 public class Type {
-    private String name;
-    private boolean isArray;
+    private final String name;
+    private final boolean isArray;
 
     public Type(String name, boolean isArray) {
         this.name = name;
         this.isArray = isArray;
-    }
-
-    public Type(JmmNode node) {
-        if(node.getKind().equals("Type")) parseType(node);
-        else {
-            this.isArray = node.getKind().contains("[]");
-            this.name = node.getKind().replace("[]", "");
-        }
-        this.name = this.name.replaceAll("'", "").replace("Identifier ", "");
-    }
-
-    public void parseType(JmmNode node){
-        List<JmmNode> children = node.getChildren();
-        this.name = children.get(0).getKind();
-        this.isArray = (children.size() == 2) && (children.get(1).getKind().equals("[]"));
     }
 
     public String getName() {
@@ -42,10 +22,38 @@ public class Type {
         return "Type [name=" + name + ", isArray=" + isArray + "]";
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (isArray ? 1231 : 1237);
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(Object obj) {
-        if((obj == null) || (obj.getClass() != Type.class)) return false;
-
-        return (isArray == ((Type) obj).isArray) && (name == ((Type) obj).getName());
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Type other = (Type) obj;
+        if (isArray != other.isArray)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
     }
+
 }
